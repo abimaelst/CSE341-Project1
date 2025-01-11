135554f05059cc3d9f2a5041690ef8671f071812
Abimael <tavares@abimael.dev.br>
2025-01-11 10:26:10 -0300

    add project base structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use('/', routes)
+
+app.listen(port, () => console.log(`Running on port ${port}`))
