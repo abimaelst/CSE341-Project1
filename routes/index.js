ac25fec323cb6a3b6a62efff73ef236f7f1cc5cc
Abimael <tavares@abimael.dev.br>
2025-01-14 22:50:23 -0300

    finish users route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
+const usersRoute = require('./users')
 
 router.get('/', (req, res) => {
   return res.send('Hellow World')
 })
+
+router.use('/users', usersRoute)
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoDb = require('./data/database')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 app.use('/', routes)
 
-app.listen(port, () => console.log(`Running on port ${port}`))
+
+
+
+mongoDb.initDb((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    app.listen(port, () => console.log(`Database is listining and Nodde is Running on port ${port}`))
+  }
+})
